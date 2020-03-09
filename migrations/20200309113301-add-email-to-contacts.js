c61cc8b3dcbac93bb64368cf206bb366779d9d42
JamesHahn3919 <james.hahn3919@gmail.com>
2020-03-09 04:42:36 -0700

    add email attribute to contact object

---
+++ migrations/20200309113301-add-email-to-contacts.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.addColumn("Contacts","Email",
+    Sequelize.STRING
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.removeColumn("Contacts","Email")
+  }
+};
